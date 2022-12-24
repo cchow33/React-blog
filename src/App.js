4961346008368c129740572dc3f44f8004c4bdce
Carmen Chow <carmendevelops@gmail.com>
2022-12-24 08:15:26 -0500

    set up router, define routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import NewPost from './components/NewPost';
+import PostPage from './components/PostPage';
+import About from './components/About';
+import Missing from './components/Missing';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Header/>
+        <Navbar/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/post" element={<NewPost />}/>
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="*" element={<Missing/>} />
+        <Footer/>
+      </Routes>
     </div>
   );
 }
